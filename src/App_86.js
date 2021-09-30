444c205661a27bed66ff63f66a7ed4696bd0e9d2
209410686 <209410686@gms.tku.edu.tw>
2021-09-30 21:32:56 +0800

    first commit

---
+++ src/App_86.js
@@ -0,0 +1,15 @@
+import Todo_86 from './components/Todo_86';
+import './App.css';
+
+function App_86() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo_86 text='Learn React' />
+      <Todo_86 text='acquaint markdown' />
+      <Todo_86 text='protect node.js' />
+    </div>
+  );
+}
+
+export default App_86;

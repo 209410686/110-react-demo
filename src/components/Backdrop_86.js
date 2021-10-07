df839fcfc640ad056603223ef7277e4364e45414
209410686 <209410686@gms.tku.edu.tw>
2021-10-07 20:22:05 +0800

    Todo Backdrop Modal

---
+++ src/components/Backdrop_86.js
@@ -0,0 +1,10 @@
+
+function Backdrop_86(props) {
+  return (
+    <div className="backdrop" onclick={props.onClose} ></div>
+  );
+}
+
+export default Backdrop_86;
+
+
--- src/components/Todo_86.js
+++ src/components/Todo_86.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import { useState } from 'react';
+import Backdrop_86 from './Backdrop_86';
+import Modal_86 from './Modal_86';
 
 function Todo_86(props) {
+  const [showModal, setShowModal] = useState();
+
+  function showModalHandler() {
+    setShowModal(true);
+  }
+
+  function closeModalHandler() {
+    setShowModal(false);
+  }
+
   return (
     <div>
       <div className='card'>
         <h2>{props.text}</h2>
         <div className='actions'>
-          <button className='btn'>Delete</button>
+          <button className='btn' onClick={showModalHandler}>
+            Delete
+          </button>
         </div>
+        {showModal && <Backdrop_86 onClick={closeModalHandler} />}
+        {showModal && (
+          <Modal_86 text='Are you sure?' onClose={closeModalHandler} />
+        )}
       </div>
     </div>
   );
